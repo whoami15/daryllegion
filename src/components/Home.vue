0d6a0dae5f95c37af706e5ec6ab6bfbcbef5f47e
Daryl Legion <whoamisnowden@gmail.com>
2021-06-02 04:41:05 +0800

    WIP

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1>{{ msg }}</h1>
+<div class="flex h-screen justify-center items-center bg-gray-100">
+    <div class="text-center">
+      <h1 class="font-mono font-semibold antialiased lg:text-5xl sm:text-3xl text-3xl text-gray-800">{{ msg }}</h1>
+      <p class="font-mono text-gray-700 font-medium">&copy; 2021</p>
+    </div>
+  </div>
 </template>
 
 <script setup>
