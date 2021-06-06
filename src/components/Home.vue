8fe51a7496672b9b7579aeb0fe24452ada427285
Daryl Legion <whoamisnowden@gmail.com>
2021-06-06 23:16:33 +0800

    WIP

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="antialiased flex h-screen justify-center items-center bg-gray-100">
     <div class="text-center">
+      <img class="w-16 md:w-28 lg:w-32 mx-auto mb-2" src="/logo.png">
       <h1 class="font-mono font-semibold lg:text-5xl sm:text-3xl text-3xl tracking-wide uppercase text-gray-800">{{ msg }}</h1>
       <p class="font-mono text-gray-500 font-semibold tracking-wide">&copy; 2021</p>
     </div>
