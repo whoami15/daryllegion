00b79b0d34f8199b026bc5e55093ab9ba2cc173b
Daryl Legion <whoamisnowden@gmail.com>
2021-06-18 06:07:33 +0800

    Add confetti

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="antialiased flex h-screen justify-center items-center bg-gray-100">
     <div class="text-center">
-      <img class="w-16 md:w-28 lg:w-32 mx-auto mb-2" src="/logo.png">
+      <img class="w-16 md:w-28 lg:w-32 mx-auto mb-2" src="/logo.png" @click='conf'>
       <h1 class="font-mono font-semibold lg:text-5xl sm:text-3xl text-3xl tracking-wide uppercase text-gray-800">{{ msg }}</h1>
       <p class="font-mono text-gray-500 font-semibold tracking-wide">&copy; 2021</p>
     </div>
@@ -9,9 +9,17 @@
 </template>
 
 <script setup>
-import { defineProps, reactive } from 'vue'
+import { defineProps, reactive, ref } from 'vue'
 
 defineProps({
   msg: String
 })
+
+function conf() {
+    confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 }
+    })
+}
 </script>
