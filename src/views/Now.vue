106a596a2c2fb86749a32f84c5f73a37a9c6b143
Daryl Legion <whoamisnowden@gmail.com>
2022-11-26 21:36:08 +0800

    Add now page

--- src/views/Now.vue
+++ src/views/Now.vue
@@ -1,5 +1,22 @@
+<script>
+</script>
+
 <template>
     <div class="flex justify-center items-center">
-      <h1>Now</h1>
+      <div class="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <div class="text-center">
+          <h1 class="font-mono font-semibold lg:text-5xl sm:text-3xl text-3xl tracking-tight uppercase text-gray-800">Now</h1>
+          <p class="mt-6 text-base text-zinc-600">This is a ”now” page and shows you what I’m currently focused on.</p>
+          <div class="mt-8 sm:mt-12">
+            <p class="mt-6 text-base text-zinc-600">Right now, I’m currently:</p>
+            <ul class="mt-6 text-base text-zinc-600 list-disc list-inside">
+                <li>Working full-time as a Web App Developer.</li>
+                <li>Taking a course AWS Certified Developer Associate DVA-C01.</li>
+            </ul>
+            <p class="mt-6 text-base text-zinc-600">Learn more about <a href="https://nownownow.com/about" target="nofollow" class="font-mono text-blue-500 font-semibold tracking-tight">now</a> what a <em class="italic">”now”</em> page is.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
 </template>
